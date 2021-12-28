01f55d690a95ff103ac1df0c3789486c0bad50dc
andresanunezt <andresanunez98@gmail.com>
2021-12-27 22:09:12 -0500

    User tests created

---
+++ tests/User.test.js
@@ -0,0 +1,18 @@
+const { TestWatcher } = require("jest");
+
+describe ('User Object Properties', () => {
+
+const user1 = new User(name)
+
+test("User is an object", () => {
+    expect(user1).toBe('object')
+})
+
+test("User is a username", () => {
+    expect(user1).toBe('username')
+})
+
+})
+
+
+
